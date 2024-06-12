3beb4f5cf567b9303379088f4debe7f91b6d3c06
ngominhduc24 <ngominhduc100@gmail.com>
2024-06-13 03:13:19 +0700

    refactor export module name mongodb connect config

--- Backend/src/config/mongodb.config.js
+++ Backend/src/config/mongodb.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // const dotenv = require("dotenv");
 // dotenv.config();
 
-const connect = async () => {
+const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI_CLOUD, {
     //   useNewUrlParser: true,
@@ -14,4 +14,4 @@ const connect = async () => {
   }
 };
 
-module.exports = connect;
+module.exports = connectMongoDB;
