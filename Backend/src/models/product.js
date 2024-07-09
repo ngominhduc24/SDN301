34263cb7435f1fa8c68955f35e7ec35184e8a10a
maidung1602 <maidung1602@gmail.com>
2024-07-09 15:11:43 +0700

    fix: product populate with category

--- Backend/src/models/product.js
+++ Backend/src/models/product.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+const Category = require('./category');
 
 // Define the Product schema for embedding
 const ProductSchema = new Schema({
   categoryId: { 
     type: Schema.Types.ObjectId, 
-    ref: 'Category'
+    ref: Category
     },
   name: { 
     type: String, 
