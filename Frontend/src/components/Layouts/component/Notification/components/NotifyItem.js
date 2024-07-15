cae8cdcbe2f4ee43ad2bc80d7d5093ad0ebfc6f7
ngominhduc24 <ngominhduc100@gmail.com>
2024-07-15 15:47:09 +0700

    wrong typo param nitify

--- Frontend/src/components/Layouts/component/Notification/components/NotifyItem.js
+++ Frontend/src/components/Layouts/component/Notification/components/NotifyItem.js
@@ -10,29 +10,30 @@ const NotifyItem = ({ notify, handleClick }) => {
 
   return (
     <WrapNotifyItem
-      className={!notify?.IsRead ? "unread" : ""}
+      className={!notify?.isRead ? "unread" : ""}
       onClick={handleClick}
     >
       <div className="avatar">
         <Avatar
           size={32}
-          src={notify?.Logo}
+          src={notify?.logo}
           icon={<UserOutlined style={{ fontSize: "16px" }} />}
         />
       </div>
       <div className="content-notify">
         <div className="hidden-text">
-          <span className="account-name">{notify?.Title}</span>
+          <span className="account-name" style={{ fontSize: "14px"}}>{notify?.title}</span>
           <br />
-          <Tooltip title={`${notify?.AccountName} - ${notify?.Content || ""}`}>
-            <span className="account-name">{notify?.AccountName}</span>{" "}
-            <span>{contentPart1}</span>{" "}
+          <Tooltip title={`${notify?.accountName} - ${notify?.content || ""}`}>
+          <span className="account-name" style={{ fontSize: "13px" }} >{notify?.accountName}: </span>{" "}
+          <span className="account-name" style={{ fontSize: "13px", fontWeight: "normal" }} >{notify?.content || ""}</span>{" "}
+          <span>{contentPart1}</span>{" "}
             <span className="package-name">
               {contentPart2}
             </span>
           </Tooltip>
         </div>
-        <div className="time"> {notify?.TimeAgo}</div>
+        <div className="time"> {notify?.timeAgo}</div>
       </div>
     </WrapNotifyItem>
   );
